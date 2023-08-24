3a66841d84a427b03384a8e99d65250bb99ccf3f
Coleman J S <8s2@narsil-gpu-login3.compute.ornl.gov>
2023-08-24 17:08:16 -0400

    added working example of yaml input with new simulation class to act as a database class for necessary variables

--- examples/main.cpp
+++ examples/main.cpp
@@ -9,53 +9,56 @@
 
 #include "grid.hpp"
 #include "movingBeam/moving_beam.hpp"
+#include "simulation.hpp"
 
-void run()
+void run(Simulation& db)
 {
     using exec_space = Kokkos::DefaultExecutionSpace;
     using device_type = exec_space::device_type;
 
-    // Create the global mesh
-    double cell_size = 20e-6;
-    std::array<double, 3> global_low_corner = { -5e-4, -5e-4, -5e-4 };
-    std::array<double, 3> global_high_corner = { 5.5e-3, 5.5e-3, 0 };
-    double init_temp = 300.0;
-
-    Grid<device_type> grid( MPI_COMM_WORLD, cell_size, global_low_corner,
-                            global_high_corner, init_temp );
-
-    // properties
-    double rho = 7500.0;
-    double specific_heat = 750.0;
-    double kappa = 25.0;
-    double alpha = kappa / ( rho * specific_heat );
-
-    // Solve heat conduction from point source
-    double Co = 0.125;       // thermal courant number for explicit stability
-    double dt = Co * cell_size * cell_size / alpha;
-    double alpha_dt_dx2 = alpha * dt / ( cell_size * cell_size );
-    double dt_rho_cp = dt / ( rho * specific_heat );
-
-    double end_time = 4e-3;
-    int numSteps = static_cast<int>( end_time / dt );
-
-    // gaussian heat source parameters (sigma is std dev of gaussian)
-    double eta = 0.35;
-    double sigma[3] = { 50e-6, 50e-6, 60e-6 };
-    double r[3] = { sigma[0] / sqrt( 2.0 ), sigma[1] / sqrt( 2.0 ),
-                    sigma[2] / sqrt( 2.0 ) };
-
-    // parameters for a moving beam in x-direction
+    // initialize a moving beam
     MovingBeam beam;
 
-    // write 10 files to create a movie
-    int num_output_steps = 10;
-    int output_interval = static_cast<int>( numSteps / num_output_steps );
-    double time = 0.0;
+    // create the global mesh
+    Grid<device_type> grid( MPI_COMM_WORLD,
+                            db.space.cell_size,
+                            db.space.global_low_corner,
+                            db.space.global_high_corner,
+                            db.space.initial_temperature );
 
+    auto local_mesh = grid.getLocalMesh();
+
+    // solution controls
+    double alpha_dt_by_dx2 = 
+        ( db.properties.thermal_diffusivity * db.time.time_step )
+      / ( db.space.cell_size * db.space.cell_size );
+
+    double dt_by_rho_cp = 
+        ( db.time.time_step )
+      / ( db.properties.density * db.properties.specific_heat );
+
+    // gaussian heat source parameters
+    double r[3] = { db.source.two_sigma[0] / sqrt( 2.0 ),
+                    db.source.two_sigma[1] / sqrt( 2.0 ),
+                    db.source.two_sigma[2] / sqrt( 2.0 ) };
+
+    double I0 = ( 2.0 * db.source.absorption ) 
+              / ( M_PI * sqrt( M_PI ) * r[0] * r[1] * r[2] );
+
+    // time stepping
+    double time = db.time.start_time;
+
+    int numSteps = static_cast<int>(
+                        ( db.time.end_time - db.time.start_time )
+                      / ( db.time.time_step ));
+
+    int output_interval = static_cast<int>(
+                        ( numSteps / db.time.num_output_steps ));
+
+    // update the temperature field
     for ( int step = 0; step < numSteps; ++step )
     {
-        time += dt;
+        time += db.time.time_step;
 
         // update beam position
         beam.move( time );
@@ -72,11 +75,19 @@ void run()
         Kokkos::deep_copy( T0, T );
 
         // Solve finite difference
-        auto local_mesh = grid.getLocalMesh();
         Cajita::grid_parallel_for(
             "local_grid_for", exec_space(), grid.getIndexSpace(),
             KOKKOS_LAMBDA( const int i, const int j, const int k )
             {
+                // calculate diffusion term
+                double laplacian =
+                    ( -6.0 * T0( i, j, k, 0 ) + T0( i - 1, j, k, 0 ) +
+                      T0( i + 1, j, k, 0 ) + T0( i, j - 1, k, 0 ) +
+                      T0( i, j + 1, k, 0 ) + T0( i, j, k - 1, 0 ) +
+                      T0( i, j, k + 1, 0 ) ) *
+                    alpha_dt_by_dx2;
+
+                // calculate source term
                 double cell_loc[3], cell_dist_to_beam[3];
                 int idx[3] = { i, j, k };
                 local_mesh.coordinates( Cajita::Cell(), idx, cell_loc );
@@ -92,34 +103,26 @@ void run()
                            ( cell_dist_to_beam[2] * cell_dist_to_beam[2] /
                              r[2] / r[2] );
 
-                // volumetric intensity of the heat source
-                double I = 2.0 * eta * beam_power /
-                           ( M_PI * sqrt( M_PI ) * r[0] * r[1] * r[2] );
-                double Q = I * exp( -f ) * dt_rho_cp;
+                double q_dot = I0 * beam_power * exp( -f ) * dt_by_rho_cp;
 
-                double laplacian =
-                    ( -6.0 * T0( i, j, k, 0 ) + T0( i - 1, j, k, 0 ) +
-                      T0( i + 1, j, k, 0 ) + T0( i, j - 1, k, 0 ) +
-                      T0( i, j + 1, k, 0 ) + T0( i, j, k - 1, 0 ) +
-                      T0( i, j, k + 1, 0 ) ) *
-                    alpha_dt_dx2;
-
-                T( i, j, k, 0 ) = T0( i, j, k, 0 ) + laplacian + Q;
+                T( i, j, k, 0 ) = T0( i, j, k, 0 ) + laplacian + q_dot;
             } );
 
-        // update boundaries and halos
+        // update boundaries
         grid.updateBoundaries();
+
+        // communicate halos
         grid.gather();
 
         // Write the current temperature field
-        if ( step % output_interval == 0 )
+        if ( output_interval && (step % output_interval == 0 ) )
         {
-            grid.output( step, step * dt );
+            grid.output( step, step * db.time.time_step );
         }
     }
 
     // Write the final temperature field
-    grid.output( numSteps, numSteps * dt );
+    grid.output( numSteps, numSteps * db.time.time_step );
 }
 
 int main( int argc, char* argv[] )
@@ -128,7 +131,9 @@ int main( int argc, char* argv[] )
     {
         Kokkos::ScopeGuard scope_guard( argc, argv );
 
-        run();
+        Simulation simulation( argc, argv );
+
+        run(simulation);
     }
     MPI_Finalize();
 
---
+++ examples/simulation.hpp
@@ -0,0 +1,185 @@
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <unistd.h>
+
+#include "yaml-cpp/yaml.h"
+
+struct Time
+{
+    double Co;
+    double start_time;
+    double end_time;
+    double time_step;
+    int num_output_steps;
+    int output_interval;
+};
+
+struct Space
+{
+    double initial_temperature;
+    double cell_size;
+    std::array<double, 3> global_low_corner;
+    std::array<double, 3> global_high_corner;
+};
+
+struct Source
+{
+    double absorption;
+    std::array<double, 3> two_sigma;
+    std::array<double, 3> r;
+};
+
+struct Properties
+{
+    double density;
+    double specific_heat;
+    double thermal_conductivity;
+    double thermal_diffusivity;
+    double latent_heat;
+};
+
+class Simulation
+{
+public:
+    Time time;
+    Space space;
+    Source source;
+    Properties properties;
+
+    // constructor
+    Simulation(int argc, char* argv[])
+    {
+        readInput(argc, argv);
+        
+        properties.thermal_diffusivity = 
+            ( properties.thermal_conductivity )
+          / ( properties.density * properties.specific_heat );
+
+        time.time_step = 
+            ( time.Co * space.cell_size * space.cell_size )
+          / ( properties.thermal_diffusivity );
+
+        write();
+
+        std::cout << "Calculated time step: " << time.time_step << std::endl;
+    }
+
+    void write()
+    {
+        std::cout << "Simulation will be performed using parameters: " << std::endl;
+
+        // Print time
+        std::cout << "Time:" << std::endl;
+        std::cout << "  Co: " << time.Co << std::endl;
+        std::cout << "  Start Time: " << time.start_time << std::endl;
+        std::cout << "  End Time: " << time.end_time << std::endl;
+        std::cout << "  Num Output Steps: " << time.num_output_steps << std::endl;
+
+        // Print space
+        std::cout << "Space:" << std::endl;
+        std::cout << "  Initial temperature: " << space.initial_temperature << std::endl;
+        std::cout << "  Cell Size: " << space.cell_size << std::endl;
+        std::cout << "  Global Low Corner:" << std::endl;
+        std::cout << "    X: " << space.global_low_corner[0] << std::endl;
+        std::cout << "    Y: " << space.global_low_corner[1] << std::endl;
+        std::cout << "    Z: " << space.global_low_corner[2] << std::endl;
+        std::cout << "  Global High Corner:" << std::endl;
+        std::cout << "    X: " << space.global_high_corner[0] << std::endl;
+        std::cout << "    Y: " << space.global_high_corner[1] << std::endl;
+        std::cout << "    Z: " << space.global_high_corner[2] << std::endl;
+
+        // Print properties
+        std::cout << "Properties:" << std::endl;
+        std::cout << "  Density: " << properties.density << std::endl;
+        std::cout << "  Specific Heat: " << properties.specific_heat << std::endl;
+        std::cout << "  Thermal Conductivity: " << properties.thermal_conductivity << std::endl;
+        std::cout << "  Latent Heat: " << properties.latent_heat << std::endl;
+
+        // Print source
+        std::cout << "Source:" << std::endl;
+        std::cout << "  Absorption: " << source.absorption << std::endl;
+        std::cout << "  two-sigma:" << std::endl;
+        std::cout << "    X: " << source.two_sigma[0] << std::endl;
+        std::cout << "    Y: " << source.two_sigma[1] << std::endl;
+        std::cout << "    Z: " << source.two_sigma[2] << std::endl;
+    }
+
+private:
+    void readInput(int argc, char* argv[])
+    {
+        const char* filename = nullptr;
+        int option;
+
+        while ((option = getopt(argc, argv, "i:")) != -1)
+        {
+            if (option == 'i') 
+            {
+                filename = optarg;
+            }
+            else
+            {
+                std::cerr << "Usage: " << argv[0]
+                          << " -i <input_yaml_file>" << std::endl;
+                return;
+            }
+        }
+
+        if (!filename)
+        {
+            std::cerr << "Input file required." << std::endl
+                      << "Usage: " << argv[0]
+                      << " -i <input_yaml_file>" << std::endl;
+            return;
+        }
+
+        try 
+        {
+            YAML::Node db = YAML::LoadFile(filename);
+
+            // Read time components
+            time.Co =
+                db["time"]["Co"].as<double>();
+            time.start_time =
+                db["time"]["start_time"].as<double>();
+            time.end_time =
+                db["time"]["end_time"].as<double>();
+            time.num_output_steps =
+                db["time"]["num_output_steps"].as<int>();
+
+            // Read space components
+            space.initial_temperature =
+                db["space"]["initial_temperature"].as<double>();
+            space.cell_size =
+                db["space"]["cell_size"].as<double>();
+            space.global_low_corner =
+                db["space"]["global_low_corner"].as<std::array<double, 3>>();
+            space.global_high_corner =
+                db["space"]["global_high_corner"].as<std::array<double, 3>>();
+
+            // Read properties components
+            properties.density =
+                db["properties"]["density"].as<double>();
+            properties.specific_heat =
+                db["properties"]["specific_heat"].as<double>();
+            properties.thermal_conductivity =
+                db["properties"]["thermal_conductivity"].as<double>();
+            properties.latent_heat =
+                db["properties"]["latent_heat"].as<double>();
+
+            // Read heat source components
+            source.absorption =
+                db["source"]["absorption"].as<double>();
+            source.two_sigma =
+                db["source"]["two_sigma"].as<std::array<double, 3>>();
+
+            source.two_sigma[0] = fabs( source.two_sigma[0] );
+            source.two_sigma[1] = fabs( source.two_sigma[1] );
+            source.two_sigma[2] = fabs( source.two_sigma[2] );
+        }
+        catch (const YAML::Exception& e)
+        {
+            std::cerr << "Error while reading YAML: " << e.what() << std::endl;
+        }
+    }
+};
